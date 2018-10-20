bd28d2ce00299a129e5b069ce568b8f4631d325d
EdiTeD's Projects <42066039+editedprojects@users.noreply.github.com>
2018-10-20 21:29:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,13 +3,14 @@ const moment = require("moment");
 const fs = require("fs");
 const sql = require("better-sqlite3");
 const client = new Discord.Client();
+const Codes = new Discord.Client();
  const prefix = "-";
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('ready', () => {
-    client.user.setActivity("ROYALE 5KK.. ",{type: 'PLAYING'})
+    client.user.setActivity("Welcome To .. Royale ' ",{type: 'PLAYING'})
 
 });
 
@@ -18,6 +19,45 @@ client.on('message',async message => {
 if(message.content === "#members") {
 return message.channel.send(`**RG Members : \`${message.guild.memberCount}\` .**`);
 }
+});
+
+
+
+const adminprefix = "#";
+const devs = ['456641975932813345'];
+Codes.on('message', message => {//for dev
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+
+if (message.content.startsWith(adminprefix + 'setgame')) {
+  Codes.user.setGame(argresult);   message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+} else
+  if (message.content.startsWith(adminprefix + 'setname')) {
+Codes.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(adminprefix + 'setavatar')) {
+Codes.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else
+if (message.content.startsWith(adminprefix + 'setT')) {
+  Rocket.user.setGame(argresult, "https://www.twitch.tv/faresgameryt");
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
+}
+
+Codes.on('message', message => {//restart
+    if(message.content === adminprefix + "ظظظظظظظظكشنسشكسشسنشمسشتس") {
+          if (!devs.includes(message.author.id)) return;
+              message.channel.send(`⚠️ **الشخص الذي اعاد تشغيل البوت ${message.author.username}**`);
+            console.log(`⚠️ جاري اعادة تشغيل البوت... ⚠️`);
+            Rocket.destroy();
+            child_process.fork(__dirname + "/bot.js");
+            console.log(`تم اعادة تشغيل البوت`);
+        }
+
+
+    });
 });
 
  client.on('message',async message => {
